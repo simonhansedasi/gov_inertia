0105a9a46813d334f6b3132bd38153d8747d553c
Simon Hans Edasi <simonhansedasi.gmail.com>
2025-08-03 22:27:30 -0700

    initial commit adding simulation setup

---
+++ gov_inertia.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+initial_conditions = {
+    'N':1000,
+    'm':0.01,
+    'k':50,
+    'G':1,
+    'H_0':3,
+    'H':0,
+    'rho':2.3,
+    'theta':1.3,
+    'F':0.2,
+    'alpha':1e-1,
+    'beta':1e-1,
+    'gamma':1e-1,
+    'delta':5,
+    'mu_0':0,
+    'sigma':1,
+    'tau':0.1,
+    'lambda':0.4,
+    'psi':0.3,
+    'epsilon':1,
+    'zeta':1e-4,
+    'eta':0.7
+}
